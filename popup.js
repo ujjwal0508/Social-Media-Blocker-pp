38b65a8aeaf26e5ddab383ec752f88e0b342f09c
ujjwal0508 <ujjwal.jindal3@gmail.com>
2020-08-13 17:41:42 +0530

    added code to block urls

--- popup.js
+++ popup.js
@@ -1,1 +1,16 @@
-console.log('popup ke andr');
+document.getElementById('add-website').onclick = async function(){
+
+    let url = document.getElementById('website').value;
+    console.log(url);
+
+    let message = {'blockUrl' : url};
+    await sendUrlToBackground(message);
+}
+
+function sendUrlToBackground(message){
+    return new Promise(function (resolve, reject) {
+        chrome.runtime.sendMessage(message, function(response) {
+            console.log(response);
+          });
+    })
+}
